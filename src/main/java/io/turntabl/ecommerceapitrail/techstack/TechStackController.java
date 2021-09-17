bfd2a9fa66fad3197ba2bcf041598f48ebbb98f7
Kelvin Morrison <kelvinninja1@gmail.com>
2021-09-17 17:18:54 +0000

    Added: update functionality & output data

--- src/main/java/io/turntabl/ecommerceapitrail/techstack/TechStackController.java
+++ src/main/java/io/turntabl/ecommerceapitrail/techstack/TechStackController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "v1/techstacks")
@@ -24,9 +25,26 @@ public class TechStackController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public List<String> createStack(@RequestBody TechStack techStack){
+    public List<Object> createStack(@RequestBody TechStack techStack){
         techStackService.addTechStack(techStack);
+        return List.of("Success", techStack);
+    }
+
+    @DeleteMapping(path = "{techStackId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Object> removeStack(@PathVariable("techStackId") Long techStackId){
+        techStackService.deleteTechStack(techStackId);
         return List.of("Success");
     }
+
+    @PutMapping(path = "{techStackId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Object> modifyStack(@PathVariable("techStackId") Long techStackId, @RequestBody Map<String, Object> change){
+        techStackService.updateTechStack(techStackId, change);
+        return List.of("Success",
+                change);
+
+    }
+
 
 }
