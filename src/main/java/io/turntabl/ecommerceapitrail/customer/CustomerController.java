2a28044f0875b32c820aac3c1cc242cfd5d33164
Kelvin Morrison <kelvinninja1@gmail.com>
2021-09-23 01:40:22 +0000

    Added: Get Customer List functionality
    Get the list of all Customers via a get request to {{baseUrl}}/v1/customers

---
+++ src/main/java/io/turntabl/ecommerceapitrail/customer/CustomerController.java
@@ -0,0 +1,25 @@
+package io.turntabl.ecommerceapitrail.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> listCustomers(){
+        return customerService.getCustomers();
+    }
+}
