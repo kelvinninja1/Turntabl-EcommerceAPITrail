20a7c4576524d37562e73bb7accd79f8edc23b6c
Kelvin Morrison <kelvinninja1@gmail.com>
2021-09-27 05:36:08 +0000

    Create ProductServiceTest.java

---
+++ src/test/java/io/turntabl/ecommerceapitrail/product/ProductServiceTest.java
@@ -0,0 +1,52 @@
+package io.turntabl.ecommerceapitrail.product;
+
+import io.turntabl.ecommerceapitrail.product.Product;
+import io.turntabl.ecommerceapitrail.product.ProductRepository;
+import io.turntabl.ecommerceapitrail.product.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepositoryUnderTest;
+    private ProductService productServiceUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        productServiceUnderTest = new ProductService(productRepositoryUnderTest);
+    }
+
+    @Test
+    void canGetProducts() {
+        //when
+        productServiceUnderTest.getProducts();
+        //then
+        verify(productRepositoryUnderTest).findAll();
+    }
+
+    @Test
+    void canAddProducts() {
+        //given
+        Product product = new Product(
+                "M1 MacBook Pro"
+        );
+        //when
+        productServiceUnderTest.addProducts(product);
+        //then
+        ArgumentCaptor<Product> productArgumentCaptor = ArgumentCaptor.forClass((Product.class));
+
+        verify(productRepositoryUnderTest).save(productArgumentCaptor.capture());
+        Product capturedProduct = productArgumentCaptor.getValue();
+
+        assertEquals(capturedProduct, product);
+    }
+}
