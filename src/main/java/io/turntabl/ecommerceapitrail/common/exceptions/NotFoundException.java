57f5b44965a092b688c9c071b75b6b3b72e9f104
Kelvin Morrison <kelvinninja1@gmail.com>
2021-10-11 22:59:27 +0000

    Added: Minor Fix

--- src/main/java/io/turntabl/ecommerceapitrail/common/exceptions/NotFoundException.java
+++ src/main/java/io/turntabl/ecommerceapitrail/common/exceptions/NotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends IllegalStateException{
-    public NotFoundException(String s) {
-        super(s);
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
     }
 
     public NotFoundException(String message, Throwable cause) {
